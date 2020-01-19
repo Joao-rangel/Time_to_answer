a55b4ed23c9e433a0a56401f4745b9695468be78
Joao Rangel <joao.vran@gmail.com>
2020-01-19 19:41:19 -0300

    password edit changes

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,20 +1,37 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+before_action :password_check, only: [:update]
+before_action :set_admin, only: [:edit, :update]
+
+
   def index
     @admins = Admin.all
   end
 
   def edit
-    @admin = Admin.find(params[:id])
   end
 
   def update
-    admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation) # limita os dados que serão enviados (segurança)
-  
-    if admin.update(params_admin) # caso haja atualização, voltar para a tabela de adm e exibir mensagem
+    if @admin.update(params_admin) # caso haja atualização, voltar para a tabela de adm e exibir mensagem
       redirect_to admins_backoffice_admins_path, notice: "Adm atualizado com sucesso."
     else #caso não consiga edição, voltar para a página edit (pág. atual)
       render :edit
     end
   end
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation) # limita os dados que serão enviados (segurança)
+  end
+
+  def password_check
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation) #remove a necessideade de senha caso ela não seja enviada
+    end
+  end
+
 end
