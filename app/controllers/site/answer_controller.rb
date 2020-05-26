5b3868af50331b83796b24b3d9bdf018e6d9f62a
Joao Rangel <joao.vran@gmail.com>
2020-05-25 23:30:44 -0300

    user backoffice minor adjusts

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,15 +1,7 @@
 class Site::AnswerController < SiteController
   def question # .where aponta uma array de hashes, .find localiza um único elemento (hash)
     @answer = Answer.find(params[:answer_id])
-    update_statistics(@answer)
+    UserStatistic.update_answers(@answer, current_user)
   end
 
-  private
-  def update_statistics(answer)
-    if user_signed_in?
-      user_statistics = UserStatistic.find_or_create_by(user: current_user)
-      answer.correct? ? user_statistics.right_questions += 1 : user_statistics.wrong_questions += 1
-      user_statistics.save
-    end
-  end
 end
--- app/models/user_statistic.rb
+++ app/models/user_statistic.rb
@@ -12,4 +12,12 @@ class UserStatistic < ApplicationRecord
   def wrong_ratio
     100 - self.right_ratio
   end
+
+  def self.update_answers(answer, current_user)
+    if !!current_user # !! negates 2 times (used to remove nil and gives false instead)
+      user_statistic = UserStatistic.find_or_create_by(user: current_user)
+      answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
+      user_statistic.save
+    end
+  end
 end
